fdbf868d3b117d1adb2860f721719316b710daf1
jjader <jamisson.junior@a3data.com.br>
2024-08-26 10:17:00 -0300

    fix: return just predict routes

--- src/routes/model/history.py
+++ src/routes/model/history.py
@@ -23,14 +23,18 @@ def convert_to_json(data):
 
     converted_data = []
     for item in data:
-        new_item = {
-            "task_id": item["task_id"],
-            "date_done": datetime_to_str(item["date_done"]),
-            "result": memoryview_to_bytes(item["result"]),
-            "status": item["status"],
-            "args": memoryview_to_bytes(item["args"]),
-            "kwargs": memoryview_to_bytes(item["kwargs"]),
-        }
+        try:
+            new_item = {
+                "task_id": item["task_id"],
+                "date_done": datetime_to_str(item["date_done"]),
+                "result": memoryview_to_bytes(item["result"]),
+                "status": item["status"],
+                "args": memoryview_to_bytes(item["args"]),
+                "kwargs": memoryview_to_bytes(item["kwargs"]),
+            }
+        except Exception:
+            new_item = {}
+            
         converted_data.append(new_item)
 
     return converted_data
@@ -46,15 +50,18 @@ async def read_history():
         dbname="mydatabase",
         user="myuser",
         password="mypassword",
-        host="localhost",
+        host="db",
         port="5432",
     )
 
     cursor = conn.cursor(cursor_factory=RealDictCursor)
 
     try:
-
-        cursor.execute("select * from celery_taskmeta")
+        cursor.execute("""
+            SELECT *
+            FROM celery_taskmeta
+            WHERE name = 'app.mlflow.tasks.predict'
+        """)
         json_result = convert_to_json(cursor.fetchall())
         return json_result
 
